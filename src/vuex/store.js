e7f5d2d90b77b550b6cf32ecdfb405a17ef5f1c6
chensuyun <867937611@qq.com>
2017-08-26 14:54:51 +0800

    waiter静态页面

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import hot_nav from '../component/order/hot_nav/hot_nav'
+import waiter from '../component/waiter/waiter'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        hot_nav
+        hot_nav,
+        waiter
     }
 })
