fb64e70fc5ca00755e9dd51ad6ac0ac6d0a95c5f
592569815 <592569815@qq.com>
2017-08-29 11:56:02 +0800

    新增中英文切换功能

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,11 +3,8 @@ import Vuex from 'vuex'
 import waiter from '../component/waiter/waiter';
 import hot_nav from '../component/order/hot_nav/hot_nav';
 import nav from '../component/nav/nav.js';
-<<<<<<< HEAD
 import detail from '../component/waiter/details/detail'
-=======
 import home from '../component/home/home.js';
->>>>>>> 37671e8cf7ccad620d0df5d2f733164ab47dc460
 
 
 Vue.use(Vuex)
@@ -17,10 +14,7 @@ export default new Vuex.Store({
         hot_nav,
         waiter,
         nav,
-<<<<<<< HEAD
-        detail
-=======
+        detail,
         home
->>>>>>> 37671e8cf7ccad620d0df5d2f733164ab47dc460
     }
 })
