252e469e3a43a95767dfea0b8ad52a7ec23754fd
chensuyun <867937611@qq.com>
2017-08-30 11:57:20 +0800

    服务员端接收厨房信息

--- src/component/waiter/details/detail.js
+++ src/component/waiter/details/detail.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import $ from 'jquery';
+import wsurl from '../../../assets/common/common.js'
+
+var _wsurl = wsurl.global.wsurl;
 
 const state = {
 	arr:[{name:'红烧牛肉面',price:98},{name:'普罗旺斯海鲜沙拉',price:97},{name:'香酥鱿鱼圈',price:90},{name:'夏日风情沙拉',price:99}],
@@ -15,7 +18,7 @@ const actions = {
 const mutations = {
 	showDetail:(data)=>{
 		console.log(666)
-		var socket = io.connect('ws://localhost:1703');
+		var socket = io.connect(_wsurl);
 			socket.on('clientOrder',function(order){
 				var orders = JSON.parse(decodeURI(order));
 				if(orders){
