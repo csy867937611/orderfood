6da7ac8c97086576520a1e090f6d6ae4ace0f1e7
wgy <1442590659@qq.com>
2017-08-31 20:01:31 +0800

    合并接单+厨房

--- src/component/kitchen/kitchen.js
+++ src/component/kitchen/kitchen.js
@@ -4,7 +4,6 @@ import wsurl from '../../assets/common/common.js'
 
 var _wsurl = wsurl.global.wsurl;
 
-
 const state = {
 	data: [],
 	all: [],
@@ -12,41 +11,20 @@ const state = {
 };
 
 const actions = {
- 	center: (store, n)=>{
- 		// console.log('actions')
- 		store.commit('center', n);
- 		console.log(store)
- 	},
- 	topover: (store, n)=>{
- 		store.commit('topover', n);
- 		console.log(store)
- 	}
+ 	
+ 
 };
 
 const mutations = {
 	center: (data) => {
-		console.log(1111111111111)
 		
-
-		var socket = io.connect(_wsurl);
-		socket.on('chilken', function(message){
-			
-		console.log(message)
-			
-		});
 	},
 	topover: (data, n)=>{
 
-		var abc = encodeURI(JSON.stringify(n));
-		var socket = io.connect(_wsurl);
-		socket.emit('success', abc);
-		socket.on('suc', function(abc){
-			console.log(JSON.parse(decodeURI(abc)));
-		})
+	
 	}
 	
 };
-
 
 
 export default {
